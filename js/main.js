3bd46c201f021580224d831a83d4a987c975f439
Nicolas Palacios <palaciosnicolas94@gmail.com>
2024-07-18 19:12:20 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Definir los precios de los productos PRUEBA
+// function sumarPrecios(producto1, producto2) {
+//     console.log(producto1 + producto2)
+// }
+
+
+
+
+// sumarPrecios(1, 2);
+// sumarPrecios(10, 2);
+// sumarPrecios(10, 20);
+// sumarPrecios(10, 220);
+
+// Definir los precios de los productos 
+function sumarPrecios() {
+    let producto1 = document.getElementById("producto1").value;
+    let producto2 = document.getElementById("producto2").value;
+    let producto3 = document.getElementById("producto3").value;
+
+    // cambiar de string a numero:
+    let numero1 = parseFloat(producto1);
+    let numero2 = parseFloat(producto2);
+    let numero3 = parseFloat(producto3);
+
+    // sumar todo
+    let suma = numero1 + numero2 + numero3;
+
+    //imprimir
+    document.getElementById("resultado").textContent = "La suma es: " + suma;
+
+}
+
